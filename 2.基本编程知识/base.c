1a1786a3a49dac7a22defcd292272483a39f6b97
Yuqi.Shao1 <yuqi.shao@geely.com>
2023-04-14 23:22:13 +0800

    update

---
+++ 2.基本编程知识/base.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main(void)
+{   
+    int i = 88;
+
+    printf("i = %d\n",i);
+    /**
+     * printf的用法
+     * %d 表示以十进制输出
+     * %x或者%X 表示以十六进制输出
+     * %o 表示以八进制输出
+    */
+    return i;
+}

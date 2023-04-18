3387a82490f97341317d04c9310560084b5ccfc1
Yuqi.Shao1 <yuqi.shao@geely.com>
2023-04-18 16:56:23 +0800

    u

--- 7.函数/3.fun.c
+++ 7.函数/3.fun.c
@@ -1,14 +1,24 @@
 # include <stdio.h>
 
+void g(void)
+{
+    printf("k = %d\n",k);
+}
 
-void f(void){
-    printf("哈哈\n");
+// k在所有函数的外面，它就是全局变量
+// 但是只有在它定义的位置后面的代码，才能使用这个变量
+int k = 30;
+
+// i、j都是局部变量
+void f(int i){
+    g();
+    printf("k = %d\n",k);
 }
 
 int main(void)
+// i也是局部变量
 {
-    // 就是将代码尽可能细粒度的封装成函数
-    f();
+   f(88);
 
     return 0;
 }
---
+++ 8.指针/6.pointer.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(void)
+{
+    int * p;
+    int i = 3;
+
+    printf("%d\n",*p);
+
+    return 0;
+}

cf989a86da4b31da20efe33fb427d0b77fe5b3a2
Yuqi.Shao1 <yuqi.shao@geely.com>
2023-04-25 19:08:56 +0800

    s

---
+++ 6.动态内存/2.malloc_强制类型转换.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+/**
+ * 【强制类型转换】
+*/
+int main(void)
+{
+     // 强制类型转换
+    double a = (int)(4.5+2.2);
+
+    printf("Value of mean : %ld\n", a );
+
+    return 0;
+}
---
+++ 6.动态内存/5.malloc_动态数组构造数组.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <mm_malloc.h>
+
+
+/**
+ * 【利用动态内存来构造数组】
+ * 
+*/
+int main(void)
+{
+    int a[5];
+
+    return 0;
+}
